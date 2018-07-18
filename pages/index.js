5b4f36966303281a7d67327aa6b81ce42bb0b422
Nianlu <nianlushu@gmail.com>
2018-07-18 19:04:06 -0400

    Added blueprintjs

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,30 @@
-export default () => <div>Welcome next</div>
+import Head from 'next/head';
+import { Navbar, Alignment, Button, Intent, Spinner } from '@blueprintjs/core';
+
+      // <link href="/node_modules/normalize.css/normalize.css" rel="stylesheet" />
+      // <link href="/node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css" rel="stylesheet" />
+      // <link href="/node_modules/@blueprintjs/core/lib/css/blueprint.css" rel="stylesheet" />
+
+export default () => 
+  <div>
+    <Head>
+      <title>Timeline</title>
+      <link href="https://unpkg.com/normalize.css" rel="stylesheet" />
+      <link href="https://unpkg.com/@blueprintjs/core/lib/css/blueprint.css" rel="stylesheet" />
+      <link href="https://unpkg.com/@blueprintjs/icons/lib/css/blueprint-icons.css" rel="stylesheet" />
+    </Head>
+    <body>
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="home" text="Home" />
+          <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
+      </Navbar>
+      <p>Welcome next!</p>
+      <Spinner intent={Intent.PRIMARY} />
+      <Button icon='refresh' />
+      <Button intent="success" text="button content" />
+    </body>
+  </div>
